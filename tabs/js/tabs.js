5f76425a5c2e312c10470839f38cf4115560567b
James Young <pronoiac@gmail.com>
2014-09-11 11:04:04 -0700

    Tabs, WIP

--- tabs/js/tabs.js
+++ tabs/js/tabs.js
@@ -1,8 +1,8 @@
-
-
 $.Tabs = function (el) {
-  
-  
+  this.$el = $(el);
+  this.$contentTabs = $(this.$el.data('content-tabs'));
+  this.$activeTab = this.$contentTabs.find(".active");
+  debugger;
 };
 
 
