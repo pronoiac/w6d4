1d79fb5172e29a0b63b616cb4478c4dba01ea627
James Young <pronoiac@gmail.com>
2014-09-11 13:31:04 -0700

    Can select new dog, working on showing TOC active

--- tabs/js/tabs.js
+++ tabs/js/tabs.js
@@ -2,13 +2,29 @@ $.Tabs = function (el) {
   this.$el = $(el);
   this.$contentTabs = $(this.$el.data('content-tabs'));
   this.$activeTab = this.$contentTabs.find(".active");
-  debugger;
+  this.$el.on('click', 'a', this.clickTab.bind(this));
 };
-
-
 
 $.fn.tabs = function () {
   return this.each(function () {
     new $.Tabs(this);
   });
+};
+
+$.Tabs.prototype.clickTab = function (event) {
+  event.preventDefault();
+  var clickedID = $(event.currentTarget).attr('href');
+  
+  // update activeTab
+  this.$contentTabs.children().removeClass("active");
+  this.$activeTab = this.$contentTabs.find(clickedID);
+  this.$activeTab.addClass('active');
+  
+  // update tabs
+  // $(event.currentTarget).siblings(".selected").removeClass("active");
+  this.$el.children().removeClass("active");
+  $(event.currentTarget).addClass('active');
+  
+  debugger;
+
 };
